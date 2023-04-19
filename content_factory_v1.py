523b157c456e9f4febd36c9c2af70049e54636d8
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2023-04-20 02:23:43 +0300

    Update content_factory_v1.py

--- content_factory_v1.py
+++ content_factory_v1.py
@@ -146,10 +146,8 @@ def main():
     st.set_page_config(page_title="Article Generator", page_icon=None, layout='centered', initial_sidebar_state='expanded')
     page_bg_img = '''
 <style>
-body {
 background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
-}
 </style>
 '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
