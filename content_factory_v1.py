66b1bc045dd7b7829b41e01e5fae15d1edddabbb
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2023-04-20 01:53:33 +0300

    Update content_factory_v1.py

--- content_factory_v1.py
+++ content_factory_v1.py
@@ -10,7 +10,137 @@ from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from html.parser import HTMLParser
 
-# Helper functions and classes remain the same
+def create_url_path(keyword):
+    url_path = keyword.lower()
+    url_path = re.sub(r"[^a-z0-9\s]+", "", url_path)  # Remove non-alphanumeric and non-space characters
+    url_path = url_path.replace(" ", "-")  # Replace spaces with hyphens
+
+    # Remove trailing hyphen if present
+    if url_path[-1] == "-":
+        url_path = url_path[:-1]
+
+    return f"/{url_path}"
+
+
+def create_full_path(domain, url_path):
+    return f"https://{domain}{url_path}"
+
+
+def generate_content(api_key, prompt, sections):
+    openai.api_key = api_key
+
+    system_message = (
+        """
+
+You are an AI language model. Your task is to follow the provided outline and ensure that the content is well-structured, SEO-friendly, and addresses the key points in each section.
+Make sure to use clear, concise language and provide practical advice, examples, and tips where applicable.
+
+""" )
+    
+    print(f"Generated prompt:\n{prompt}\n")
+
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": prompt}
+    ]
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=3200,
+        messages=messages
+    )
+
+    response = completion.choices[0].message.content.strip()
+    return response
+
+
+def generate_related_links(df, current_topic):
+    current_category = df.loc[df['topic'] == current_topic, 'category'].values[0]
+    current_full_path = df.loc[df['topic'] == current_topic, 'full path'].values[0]
+    related_links = df[df['category'] == current_category][['topic', 'full path']]
+    
+    # Filter out the self-link by comparing the full paths
+    related_links = related_links[related_links['full path'] != current_full_path]
+
+    return related_links.to_dict('records')
+
+
+def generate_article(topic, sections, related_links, definition_only=False):
+    if definition_only:
+        prompt = (
+            "Please provide a short, clear and concise definition for the marketing term '{}'."
+        ).format(topic)
+    else:
+        if related_links:
+            related_links_prompt = (
+    """
+
+    In your HTML output, incorporate the following related links into the article text by using relevant anchor text when applicable. 
+    If a link is not directly relevant to the text, include it in the 'related terms' section. Here are the related links to incorporate:\n\n{}.
+
+    """
+            ).format(", ".join([f"{rl['topic']} ({rl['full path']})" for rl in related_links]))
+        else:
+            related_links_prompt = ""
+
+        prompt = (
+            """
+
+Please write an informative article about the marketing term '{}' following the given outline:\n\n{}\n
+Please provide the output in semantic HTML format (there is no need for the H1). {}"""
+        ).format(topic, "\n".join(str(sec) for sec in sections), related_links_prompt)
+
+    article = generate_content(api_key, prompt, sections)
+    return article
+
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.text = []
+        self.current_tag = None
+        self.parent_tag = None
+
+    def handle_starttag(self, tag, attrs):
+        self.current_tag = tag
+        if tag in ["ul", "ol"]:
+            self.parent_tag = tag
+
+    def handle_endtag(self, tag):
+        if tag == self.parent_tag:
+            self.parent_tag = None
+        self.current_tag = None
+
+    def handle_data(self, data):
+        if self.current_tag in ["h2", "h3", "h4", "p"]:
+            self.text.append({"type": self.current_tag, "content": data.strip()})
+        elif self.current_tag == "li":
+            self.text.append({"type": self.current_tag, "content": data.strip(), "parent": self.parent_tag})
+
+def save_article_as_docx(filename, title, definition, content):
+    # Parse the HTML content
+    parser = MyHTMLParser()
+    parser.feed(content)
+    parsed_content = parser.text
+
+    # Create a new DOCX document
+    doc = Document()
+    doc.add_heading(title, level=1)
+    doc.add_paragraph(definition)
+
+    # Add parsed content to the DOCX document
+    for item in parsed_content:
+        if item["type"] in ["h2", "h3", "h4"]:
+            level = int(item["type"][1])
+            doc.add_heading(item["content"], level=level)
+        elif item["type"] == "p":
+            doc.add_paragraph(item["content"])
+        elif item["type"] == "li":
+            style = "ListBullet" if item["parent"] == "ul" else "ListNumber"
+            doc.add_paragraph(item["content"], style=style)
+
+    # Save the document to a file
+    doc.save(filename)
 
 def main():
     st.set_page_config(page_title="Article Generator", page_icon=None, layout='wide', initial_sidebar_state='expanded')
