cfb1bc839212c43e38d32d4684199768d3bbe731
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2023-04-20 02:18:43 +0300

    add bg image

--- content_factory_v1.py
+++ content_factory_v1.py
@@ -144,6 +144,16 @@ def save_article_as_docx(filename, title, definition, content):
 
 def main():
     st.set_page_config(page_title="Article Generator", page_icon=None, layout='centered', initial_sidebar_state='expanded')
+    page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+   
 
     st.title("AI Content Factory")
     st.write("A powerful AI-driven content generation tool for creating high-quality articles at scale.")
