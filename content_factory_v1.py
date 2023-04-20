74c6e9bd4095b2058703465e3b2e5609f5d4ed1f
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2023-04-20 13:27:39 +0300

    Adding cache

--- content_factory_v1.py
+++ content_factory_v1.py
@@ -65,6 +65,7 @@ def generate_related_links(df, current_topic):
     return related_links.to_dict('records')
 
 
+@st.cache(show_spinner=False, ttl=86400)
 def generate_article(api_key, topic, sections, related_links, definition_only=False):
     if definition_only:
         prompt = (
