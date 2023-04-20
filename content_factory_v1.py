07c66fecd852e2156993cbfd1755deb0ebe7d86d
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2023-04-20 10:04:22 +0300

    Update content_factory_v1.py

--- content_factory_v1.py
+++ content_factory_v1.py
@@ -143,7 +143,7 @@ def save_article_as_docx(filename, title, definition, content):
     doc.save(filename)
 
 def main():
-    st.set_page_config(page_title="Article Generator", page_icon=None, layout='centered', initial_sidebar_state='expanded')
+    st.set_page_config(page_title="AI Content Factory", page_icon=None, layout='centered', initial_sidebar_state='expanded')
 
     st.title("AI Content Factory")
     st.write("A powerful AI-driven content generation tool for creating high-quality articles at scale.")
